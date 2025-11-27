1d3db40c01f1f7b30fa6b2e2941eaf97b313dcc8
S0ra-0 <79579919+S0ra-0-0@users.noreply.github.com>
2025-11-27 16:01:53 +0100

    small conveyer belt change

--- Cafe CATastrophy/Assets/Dev Assets/Scripts/ConveyerBelt.cs
+++ Cafe CATastrophy/Assets/Dev Assets/Scripts/ConveyerBelt.cs
@@ -5,6 +5,14 @@ public class ConveyerBelt : MonoBehaviour
     [SerializeField] private float speed = 2f;
     [SerializeField] private Vector3 direction = Vector3.forward;
 
+    public InventoryItems inventoryItem;
+    public InventoryManager inventoryManager;
+    public InteractAbility interactScript;
+
+    public bool inRange;
+    public InventoryItems itemOnCounter;
+    public Transform[] counterPosition;
+    private float cooldownTimer = 0;
 
     private void OnCollisionStay(Collision collision)
     {
@@ -13,5 +21,20 @@ public class ConveyerBelt : MonoBehaviour
             Rigidbody rb = collision.collider.attachedRigidbody;
             rb.MovePosition(rb.position + direction.normalized * speed * Time.deltaTime);
         }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            interactScript = other.gameObject.GetComponent<InteractAbility>();
+            inventoryManager = other.gameObject.GetComponent<InventoryManager>();
+            inRange = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        inRange = false;
     }
 }
