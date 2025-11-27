6df08177041e524503fa8ea61fb40243544b3cb1
Emilia <emiliadeboer1@gmail.com>
2025-11-27 20:37:08 +0100

    prefab array put in playerManager

--- Cafe CATastrophy/Assets/Dev Assets/Scripts/PlayerManagerGD1.cs
+++ Cafe CATastrophy/Assets/Dev Assets/Scripts/PlayerManagerGD1.cs
@@ -5,8 +5,11 @@ public class PlayerManagerGD1 : MonoBehaviour
 {
     public Transform[] SpawnPoints;
 
+    public GameObject[] PlayerPrefabs;
+
     public void SetSpawn(PlayerInput _player)
     {
         _player.gameObject.transform.position = SpawnPoints[_player.playerIndex].position;
+        
     }
 }
