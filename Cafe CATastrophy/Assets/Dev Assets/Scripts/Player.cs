0ef24f6e57b856c4015d5d552298a0b93c60157c
Emilia <emiliadeboer1@gmail.com>
2025-12-02 15:11:46 +0100

    player inv ui

--- Cafe CATastrophy/Assets/Dev Assets/Scripts/Player.cs
+++ Cafe CATastrophy/Assets/Dev Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerGD1 : MonoBehaviour
 {
@@ -15,9 +16,14 @@ public class PlayerGD1 : MonoBehaviour
     [SerializeField] private Transform itemHoldPosistion;
     private GameObject heldItem;
 
+    public Image invImage;
+    public InventoryManager inventoryManager;
+
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        inventoryManager = GetComponent<InventoryManager>();
 
         if (PlayerInputObj.playerIndex < Colors.Length)
         {
@@ -32,6 +38,10 @@ public class PlayerGD1 : MonoBehaviour
 
     void Update()
     {
+        //code for player inventory in UI
+        
+
+
         if (!isStunned)
         {
             Vector2 input = PlayerInputObj.actions["Move"].ReadValue<Vector2>();
--- Cafe CATastrophy/Assets/Dev Assets/Scripts/PlayerManagerGD1.cs
+++ Cafe CATastrophy/Assets/Dev Assets/Scripts/PlayerManagerGD1.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerManagerGD1 : MonoBehaviour
 {
     public Transform[] SpawnPoints;
     public GameObject[] PlayerPrefabs;
-    public void SetSpawn(PlayerInput _player)
+
+
+    public void SetSpawn(PlayerInput player)
     {
-        _player.gameObject.transform.position = SpawnPoints[_player.playerIndex].position;
+        player.gameObject.transform.position = SpawnPoints[player.playerIndex].position;
+
         OrderManager.Instance.StartOrders();
         GameManager.Instance.StartTimer();
 
