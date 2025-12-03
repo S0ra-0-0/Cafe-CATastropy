dc0f3425935f0a1e2e25a4a639a5bd47c3cd756d
Emilia <emiliadeboer1@gmail.com>
2025-12-03 10:30:33 +0100

    playermanager

--- Cafe CATastrophy/Assets/Dev Assets/Scripts/PlayerManagerGD1.cs
+++ Cafe CATastrophy/Assets/Dev Assets/Scripts/PlayerManagerGD1.cs
@@ -6,14 +6,14 @@ public class PlayerManagerGD1 : MonoBehaviour
     public Transform[] SpawnPoints;
     public GameObject[] PlayerPrefabs;
 
+    public PlayerInputManager playerInputManager;
+
     public void SetSpawn(PlayerInput _player)
     {
-        Debug.Log(_player.playerIndex + " spawned at " + SpawnPoints[_player.playerIndex].position);
-
-
         _player.gameObject.transform.position = SpawnPoints[_player.playerIndex].position;
 
         OrderManager.Instance.StartOrders();
         GameManager.Instance.StartTimer();
     }
+
 }
