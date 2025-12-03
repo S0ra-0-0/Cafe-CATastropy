5acfb65766d596d5cae183a3f5482b11f51b7d46
Emilia <emiliadeboer1@gmail.com>
2025-12-03 09:31:47 +0100

    edit to spawn points

--- Cafe CATastrophy/Assets/Dev Assets/Scripts/PlayerManagerGD1.cs
+++ Cafe CATastrophy/Assets/Dev Assets/Scripts/PlayerManagerGD1.cs
@@ -6,12 +6,13 @@ public class PlayerManagerGD1 : MonoBehaviour
     public Transform[] SpawnPoints;
     public GameObject[] PlayerPrefabs;
 
-
     public void SetSpawn(PlayerInput player)
     {
+        int index = player.playerIndex;
+
+        GameObject playerObj = Instantiate(PlayerPrefabs[index], SpawnPoints[index].position, SpawnPoints[index].rotation);
+
         OrderManager.Instance.StartOrders();
         GameManager.Instance.StartTimer();
-
-        player.gameObject.transform.position = SpawnPoints[player.playerIndex +1].transform.position;
     }
 }
